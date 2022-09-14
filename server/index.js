56664555315546d23c881840bc87a3d9b17a8aea
Evgeniyilin <ievgeniy@live.ru>
2022-09-14 12:35:35 +0500

    add pack express, mongoose, config, nodemon (dev). Config and start server.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express()
+const PORT = config.get('serverPort')
+const start = () => {
+    try {
+
+        app.listen(PORT, () => {
+            console.log('Server started on PORT ', PORT)
+        })
+    } catch (e) {
+        
+    }
+}
+
+start()
